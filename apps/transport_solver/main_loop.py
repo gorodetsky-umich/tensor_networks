e9beb0290c2b8ef430312e87aa23365135d02bf6
Alex <alex@alexgorodetsky.com>
2024-09-13 15:03:12 -0400

    more refactor

--- apps/transport_solver/main_loop.py
+++ apps/transport_solver/main_loop.py
@@ -1,10 +1,60 @@
 """Main Loop."""
 import pathlib
 import logging
+from typing import List
 
 from load_config import Config
 import solver
 import problems
+
+def plot_ranks_compressions(disc: solver.Discretization,
+                            sols: List[solver.Solution]):
+    """Plot the ranks assuming a TT compression.
+
+    TODO:
+    Add general function to tensor network to get number of parmeters!
+    """
+
+    max_ranks = [np.max(s.sol.ranks()) for s in sols]
+    mean_ranks = [np.mean(s.sol.ranks()) for s in sols]
+
+    dim = disc.dimension
+    if dim == 1:
+        total_unknowns = disc.x.shape[0] * disc.theta.shape[0] \
+            * disc.psi.shape[0]
+    elif dim == 2:
+        total_unknowns = disc.x.shape[0] * disc.y.shape[0] * \
+            disc.theta.shape[0] * disc.psi.shape[0]
+
+
+    storage = [None] * len(sols)
+    for ii, sol in enumerate(sols):
+        ranks = sol.sol.ranks()
+        num_params = ranks[0] * disc.x.shape[0] + \
+            ranks[0] * ranks[1] * disc.theta.shape[0] + \
+            ranks[1] * disc.psi.shape[0]
+        storage[ii] = num_params
+
+    compression_ratio = total_unknowns / np.array(storage)
+    inds = np.arange(1, len(sols)+1)
+    cumulative_unknowns = total_unknowns * inds
+    cumulative_storage = np.cumsum(storage)
+    cumulative_compression = cumulative_unknowns / cumulative_storage
+
+    fig, axs = plt.subplots(1, 2)
+    axs[0].plot(max_ranks, '--k', label='max')
+    axs[0].plot(mean_ranks, '-k', label='mean')
+    axs[0].set_xlabel('Iteration')
+    axs[0].set_ylabel('Rank')
+    axs[0].legend()
+
+    axs[1].semilogy(compression_ratio, '--k', label='per step')
+    axs[1].semilogy(cumulative_compression, '-k', label='cumulative')
+    axs[1].set_xlabel('Iteration')
+    axs[1].set_ylabel('Compression Ratio')
+    axs[1].legend()
+    plt.tight_layout()
+    return fig, axs
 
 def main_loop(config: Config, logger: logging.Logger, save_dir: pathlib.Path):
 
@@ -22,12 +72,11 @@ def main_loop(config: Config, logger: logging.Logger, save_dir: pathlib.Path):
     sol[0] = problem.compute_aux_quantity(sol[0], disc)
 
     ax_o = problem.plot_per_step_overlay(sol[0], disc)
-    
+
     # time_step_plot, overlay_time_plot, final_plot = get_plots(disc, config)
 
 
-    if time_step_plot is not None:
-        fig, axs = time_step_plot(sol[0])
+    axs = problem.plot_per_step(sol[0], disc)
 
     time = 0.0
     for ii in (pbar := tqdm(range(1, config.solver.num_steps + 1))):
@@ -64,27 +113,26 @@ def main_loop(config: Config, logger: logging.Logger, save_dir: pathlib.Path):
             ax_o = problem.plot_per_step_overlay(
                 sol[ii],
                 disc,
-                ax=ax_o,
-                save=save_dir / f'{config.problem}_{ii}_overlay_{sol[ii].time}.pdf'
+                ax=ax_o
+            )
+            ax_o.get_figure.savefig(
+                save_dir / f'{config.problem}_{ii}_overlay_{sol[ii].time}.pdf'
             )
 
-            if time_step_plot is not None:
-                fig, _ = time_step_plot(sol[ii])
-                fig.savefig(
+            ax = problem.plot_per_step(sol[ii], disc)
+            if ax != None:
+                ax.get_figure().savefig(
                     save_dir / f'{config.problem}_{ii}_{sol[ii].time}.pdf'
                 )
 
-    if ax_o is not None:        
-        ax_o.get_figure().savefig(
-            save_dir / f'{config.problem}_overlay_final.pdf'
-        )
 
-        
     logger.info("Final time: %r",sol[-1].time)
     plot_ranks_compressions(disc, sol)
     plt.savefig(save_dir / f'{config.problem}_ranks_compression.pdf')
 
     axs = problem.plot_all_sols(sol, disc)
     if axs != None:
-        axs.get_figure().savefig(save_dir / f'{config.problem}_comparison_to_analytical.pdf')
+        axs.get_figure().savefig(
+            save_dir / f'{config.problem}_comparison_to_analytical.pdf'
+        )
     plt.show()
--- apps/transport_solver/problems.py
+++ apps/transport_solver/problems.py
@@ -5,6 +5,8 @@ import abc
 from dataclasses import dataclass
 
 import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
 
 import pytens
 
@@ -53,6 +55,17 @@ class Problem(abc.ABC):
         pass
 
     @abc.abstractmethod
+    def plot_per_step_overlay(self, sol, disc, ax=None):
+        pass
+
+    def plot_per_step(
+            self,
+            sol: solver.Solution,
+            disc: solver.Discretization
+    ) -> Optional[matplotlib.axes.Axes]:
+        pass
+
+    @abc.abstractmethod
     def plot_all_sols(self):
         pass
 
@@ -87,18 +100,65 @@ class Hohlraum(Problem):
             disc: solver.Discretization
     ) -> solver.Solution:
         """Compute auxiliary quantities."""
-        if self.geometry.dimension == 1:
+        if self.config.geometry.dimension == 1:
             sol.aux['mean_intensity'] = mean_intensity(
                 self.indices, disc, sol
             )
-        
+
         return sol
 
-    def analytic_sol(self):
+    def analytic_sol_2d(
+            self,
+            time: float,
+            disc: solver.Discretization
+    ) -> np.ndarray:
+
+        jleft = np.zeros((disc.y.shape[0], disc.x.shape[0])) + 1e-16
+        jbottom = np.zeros((disc.y.shape[0], disc.x.shape[0])) + 1e-16
+
+        ind_x = disc.x < sol.time
+        xx_eta_x = xx[:, ind_x]
+        yy_eta_x = yy[:, ind_x]
+        term1 = yy_eta_x / np.sqrt(sol.time**2 - xx_eta_x**2)
+        term1[term1 > 1] = 1
+        eta_x = np.arccos(term1)
+        jleft[:, ind_x] = 0.5 - \
+            (np.pi - eta_x) * xx_eta_x  /(2 * np.pi * sol.time) \
+            - 0.5 / np.pi * \
+            np.arcsin(
+                xx_eta_x * np.sin(eta_x) /
+                np.sqrt(xx_eta_x**2 + yy_eta_x**2 + 1e-16)
+            )
+
+        ind_y = disc.y < sol.time
+        xx_eta_y = xx[ind_y, :]
+        yy_eta_y = yy[ind_y, :]
+        term2 = xx_eta_y / np.sqrt(sol.time**2 - yy_eta_y**2)
+        term2[term2 > 1] = 1
+        eta_y = np.arccos(term2)
+        jbottom[ind_y, :] = 0.5 - \
+            (np.pi - eta_y) * yy_eta_y  /(2 * np.pi * sol.time) \
+            - 0.5 / np.pi * \
+            np.arcsin(
+                yy_eta_y * np.sin(eta_y) /
+                np.sqrt(xx_eta_y**2 + yy_eta_y**2 + 1e-16)
+            )
+
+        j = jleft[1:, 1:] + jbottom[1:, 1:]
+        return j
+
+
+    def analytic_sol(
+            self,
+            time: float,
+            disc: solver.Discretization
+    ) -> np.ndarray:
+        if self.config.geometry.dimension == 2:
+            return self.analytic_sol_2d(time, disc)
         pass
 
-    def plot_all_sols_1d(self, sols: List[Solution],
-                         disc: Discretization):
+    def plot_all_sols_1d(self, sols: List[solver.Solution],
+                         disc: solver.Discretization):
         fig, axs = plt.subplot_mosaic(
             [
                 ['numerical', 'analytical', 'color'],
@@ -151,38 +211,78 @@ class Hohlraum(Problem):
         axs['error'].set_title('Error')
         fig.colorbar(pos, cax=axs['color2'])
         return axs
-        
+
     def plot_all_sols(self,
                       sols: List[solver.Solution],
-                      disc: Discretization):
+                      disc: solver.Discretization):
 
         if self.config.geometry.dimension == 1:
             return self.plot_all_sols_1d(sols, disc)
         else:
             return None
-        
 
-    def plot_per_step_overlay(self, sol, disc, ax=None,
-                              save: Optional[pathlib.Path]=None):
+
+    def plot_per_step_overlay(self, sol, disc, ax=None):
         if ax is None:
-            fig, ax = plt.figure()
+            fig, ax = plt.subplots(1, 1)
         if disc.dimension == 1:
             y = sol.aux['mean_intensity']
             ax.plot(disc.x[1:], y[1:], '-', color='gray', alpha=0.2)
             ax.set_xlabel(r'Space $x$')
             ax.set_ylabel(r'Mean intensity $J$')
-            
-            if save:
-                fig = ax.get_figure()
-                if save:
-                    fig.savefig(save)
+
         else:
             return None
 
         return ax
 
-    def plot_per_step(self):
-        pass
+    def plot_per_step(self, sol: solver.Solution, disc: solver.Discretization
+                      ) -> Optional[matplotlib.axes.Axes]:
+        if disc.dimension == 1:
+            return None
+        else:
+            fig, axs = plt.subplot_mosaic(
+                [
+                    ['numerical', 'analytical', 'color'],
+                    ['error', 'error', 'color2']
+                ],
+                width_ratios = [0.45, 0.45, 0.1],
+                layout='constrained'
+            )
+
+            y = sol.aux['mean_intensity']
+            xx, yy = np.meshgrid(disc.x, disc.y)
+            out = y.reshape((disc.x.shape[0], disc.y.shape[0]))
+            levels = np.linspace(0, 1, 10)
+            pos = axs['numerical'].contourf(xx[1:, 1:],
+                                            yy[1:, 1:],
+                                            out.T[1:, 1:],
+                                            levels=levels)
+            axs['numerical'].set_xlabel(r'$x$')
+            axs['numerical'].set_ylabel(r'$y$')
+
+
+
+            axs['analytical'].contourf(xx[1:, 1:],
+                                       yy[1:, 1:],
+                                       j,
+                                       levels=levels)
+
+            fig.colorbar(pos, cax=axs['color'])
+
+            j = self.get_analytic_sol(sol.time, disc)
+
+            err = np.abs(out.T[1:, 1:] - j) + 1e-16
+            cmap = plt.colormaps["bone"]
+            pos = axs['error'].contourf(xx[1:, 1:],
+                                        yy[1:, 1:],
+                                        err, cmap=cmap)
+            axs['error'].set_xlabel('Time')
+            axs['error'].set_ylabel('Space')
+            axs['error'].set_title('Error')
+            fig.colorbar(pos, cax=axs['color2'])
+            return axs
+
 
 
 def load_problem(config: Config) -> Problem:
